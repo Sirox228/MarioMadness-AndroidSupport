00afbcfdc520f8e2959ff4f40fcfe46e08557cf7
DJjoteeYT <95763645+DJjoteeYT@users.noreply.github.com>
2022-10-01 13:30:04 +0200

    Upload powerdown data

---
+++ assets/preload/data/powerdown/script.lua
@@ -0,0 +1,95 @@
+
+local xx = 380;
+local yy = 350;
+local yyh = 350;
+local xx2 = 1120;
+local yy2 = 550;
+local ofs = 30;
+local ofs2 = 120;
+local followchars = true;
+local del = 0;
+local del2 = 0;
+local angleshit = 1;
+local anglevar = 1;
+
+
+function onBeatHit()
+    if curBeat > 359 then
+        if curBeat < 425 then
+        if curBeat % 2 == 0 then
+            angleshit = anglevar;
+        else
+            angleshit = -anglevar;
+        end
+        setProperty('camHUD.angle',angleshit*3)
+        setProperty('camGame.angle',angleshit*3)
+        doTweenAngle('turn', 'camHUD', angleshit, stepCrochet*0.002, 'circOut')
+        doTweenX('tuin', 'camHUD', -angleshit*8, crochet*0.001, 'linear')
+        doTweenAngle('tt', 'camGame', angleshit, stepCrochet*0.002, 'circOut')
+        doTweenX('ttrn', 'camGame', -angleshit*8, crochet*0.001, 'linear')
+    end
+end
+
+function onUpdate()
+	if del > 0 then
+		del = del - 1
+	end
+	if del2 > 0 then
+		del2 = del2 - 1
+	end
+    if followchars == true then
+        if mustHitSection == false then
+            if getProperty('dad.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx-ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx+ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx,yy-ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx,yy+ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'Hey' then
+                triggerEvent('Camera Follow Pos',xx-ofs,yyh)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singRIGHT-alt' then
+                triggerEvent('Camera Follow Pos',xx+ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singUP-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy-ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singDOWN-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy+ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'idle-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+        else
+
+            if getProperty('boyfriend.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx2-ofs,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx2+ofs,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx2,yy2-ofs)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx2,yy2+ofs)
+            end
+	    if getProperty('boyfriend.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx2,yy2)
+            end
+        end
+    else
+        triggerEvent('Camera Follow Pos','','')
+    end
+    
+end
+end
